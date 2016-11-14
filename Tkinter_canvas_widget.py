d09c9e040869a5dfbd48ba3ca2294dc2dd9f60ff
Ben Woodfield <raser.13@hotmail.com>
2016-11-14 00:37:55 +0000

    Add files via upload

---
+++ Tkinter_canvas_widget.py
@@ -0,0 +1,5 @@
+Python 2.7.11rc1 (v2.7.11rc1:82dd9545bd93, Nov 21 2015, 23:25:27) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/Tonil/Desktop/Python - for USB/tkinter_github.py =====
+>>> 
